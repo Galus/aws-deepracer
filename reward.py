b1d1ad6b5443cf8bfec2566ae6d756b5ac0a10ab
Galus <mgalus@gmail.com>
2019-09-23 22:57:37 -0500

    Update reward.py
    sheeet

--- reward.py
+++ reward.py
@@ -7,12 +7,19 @@ def reward_function(params):
     track_width = params['track_width']
     distance_from_center = params['distance_from_center']
     speed = params['speed']
+    progress = params['progress']
     
     # CONSTS
     MIN_REWARD = 1e-3
     MAX_REWARD = 1e2
     DIRECTION_THRESHOLD = 10.0
     ABS_STEERING_THRESHOLD = 30
+    max_speed = 8.0
+    speed_slug = 0.01 * max_speed
+    speed_slow = 0.11 * max_speed
+    speed_moderate = 0.33 * max_speed
+    speed_fast = 0.80 * max_speed
+    speed_extreme = max_speed
     
     def on_track_reward(current_reward, on_track):
         if not on_track:
@@ -42,41 +49,44 @@ def reward_function(params):
             current_reward = 1e-3           # likely crashed/ close to off track
         return current_reward
         
-    # promote complete laps
-    if progress == 100.0:
-        reward += 999.0
+    def complete_lap(current_reward, progress):
+        # promote complete laps
+        if progress == 100.0:
+            current_reward *= 1000
     
-    # promote speediness
-    max_speed = 8.0
-    speed_slug = 0.01 * max_speed
-    speed_slow = 0.11 * max_speed
-    speed_moderate = 0.33 * max_speed
-    speed_fast = 0.80 * max_speed
-    speed_extreme = max_speed
-    
-    if speed <= speed_slug:
-        reward -= 0.25          # bad boy
-    elif speed <= speed_slow:
-        reward += 0.11
-    elif speed <= speed_moderate:
-        reward += 0.33
-    elif speed <= speed_fast:
-        reward += 0.99
-    else:
-        # slow down
-        reward -= 0.20          # do not let it get too fast
+    def like_sanic(current_reward, speed):
+        # promote speediness
+        if speed <= speed_slug:
+            reward *= 0.9          # bad boy
+        elif speed <= speed_slow:
+            reward *= 1.1
+        elif speed <= speed_moderate:
+            reward *= 1.33
+        elif speed <= speed_fast:
+            reward *= 1.5
+        else:
+            reward = MIN_REWARD          ## slow down do not let it get too fast
         
     def straight_line_reward(current_reward, steering, speed):
         # Positive reward if the car is in a straight line going fast
-        if abs(steering) < 0.1 and speed > 3:
+        if abs(steering) < 0.1 and speed > speed_moderate:
             current_reward *= 1.2
         return current_reward
+    
+    def throttle_reward(current_reward, speed, steering):
+        # Decrease throttle while steering
+        if speed > speed_moderate - (0.4 * abs(steering)):
+            current_reward *= 0.8
+        return current_reward
 
-    reward = on_track_reward(reward, on_track)
-    reward = distance_from_center_reward(reward, track_width, distance_from_center)
-    reward = straight_line_reward(reward, steering, speed)
-    reward = direction_reward(reward, waypoints, closest_waypoints, heading)
-    reward = steering_reward(reward, steering)
-    reward = throttle_reward(reward, speed, steering)
-    reward = distance_from_center_reward(reward, track_width, distance_from_center)
+    reward = on_track_reward(reward, on_track)  # stay on track
+    reward = distance_from_center_reward(reward, track_width, distance_from_center) # stay center
+    reward = complete_lap(reward, progress) # promote full laps
+    reward = like_sanic(reward, speed)  # promote going moderate speed, penalize sluggish and too fast
+    reward = straight_line_reward(reward, steering, speed)  # promote going fast in a straight line
+    reward = throttle_reward(reward, speed, steering) # penalize turning when going moderate speed
+    # need to implement v
+    #reward = steering_reward(reward, steering)
+    #
+    #reward = distance_from_center_reward(reward, track_width, distance_from_center)
     return float(reward)
